ed0fb99590c1c37e82b88859ce28e83e812bb4c5
John <JLSenar@gmail.com>
2019-12-13 15:04:11 -0800

    Adds Card Component and testData

---
+++ src/stories/2-Card.stories.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { Card } from '../components/Card';
+import { data } from '../testData';
+
+export default {
+  title: 'Card',
+};
+
+export const card = () => <Card restaurant={data[2]} />;
