cb2e032d1c9208ab5b0bee4b71daaf7da43e2643
John <JLSenar@gail.com>
2019-12-13 15:04:11 -0800

    Adds Card Component and testData

---
+++ src/components/Card.jsx
@@ -0,0 +1,60 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import styled from 'styled-components';
+
+const cardWidth = '400px'
+
+const CardContainer = styled.div`
+  width: ${cardWidth};
+  box-shadow: 0 4px 6px 0 hsla(0, 0%, 0%, 0.2);
+  border-radius: 12px 12px 0px 0px;
+`;
+
+const Square = styled.div`
+  width: 100%;
+  height: ${cardWidth};
+  border-radius: inherit;
+`;
+
+const Img = styled.img`
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+  border-radius: inherit;
+`;
+
+const DetailRow = styled.div`
+  display: flex;
+  justify-content: space-between;
+`;
+
+const Name = styled.h3`
+  color: red;
+`;
+
+export const Card = (props) => {
+  const { restaurant } = props;
+  const { name, rating, price, review_count: reviewCount, photos} = restaurant;
+  return (
+    <CardContainer>
+      <Square>
+        <Img src={photos[0]}/>
+      </Square>
+      <Name>{name}</Name>
+      <DetailRow>
+        <span>{rating} stars</span>
+        <span>{price}</span>
+        <span>{reviewCount} reviews</span>
+      </DetailRow>
+    </CardContainer>
+  );
+}
+
+Card.propTypes = {
+  restaurant: PropTypes.shape({
+    name: PropTypes.string,
+    rating: PropTypes.number,
+    review_count: PropTypes.number,
+    photos: PropTypes.arrayOf(PropTypes.string),
+  }).isRequired,
+};
