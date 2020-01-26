97ad9c5aaa488db0861d153c5943e7b4476a14cc
John <JLSenar@gail.com>
2020-01-25 22:07:57 -0800

    Separated Like and Dislike animations

--- src/components/CardDeck.jsx
+++ src/components/CardDeck.jsx
@@ -2,6 +2,8 @@ import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
 import Card from './Card';
+import { TransitionGroup, CSSTransition } from 'react-transition-group';
+
 
 const ButtonRow = styled.div`
   display: flex;
@@ -14,35 +16,61 @@ const Deck = styled.div`
   width: 400px;
 `;
 
+function Buttons({ setCurrentIdx, setLiked, setDisliked }) {
+  return (
+    <ButtonRow>
+      <button
+        onClick={async () => {
+          await setDisliked(true);
+          await setLiked(false);
+          setCurrentIdx((idx) => idx + 1);
+        }}
+        >
+        Dislike
+      </button>
+      <button
+        onClick={async () => {
+        await setLiked(true);
+        await setDisliked(false);
+        setCurrentIdx((idx) => idx + 1);
+      }}
+        >
+        Like
+      </button>
+    </ButtonRow>
+  );
+}
+
 export default function CardDeck(props) {
   const { restaurants, size } = props;
   let [currentIdx, setCurrentIdx] = useState(0);
+  let [liked, setLiked] = useState(false);
+  let [disliked, setDisliked] = useState(false);
   let restaurant = restaurants[currentIdx];
 
-  if (restaurant) {
-    return (
-      <Deck>
-        <Card restaurant={restaurant} />
-        
-        <ButtonRow>
-          <button
-            onClick={() => setCurrentIdx((idx) => idx + 1)}
-            >
-            Dislike
-          </button>
-          <button
-            onClick={() => setCurrentIdx((idx) => idx + 1)}
-            >
-            Like
-          </button>
-        </ButtonRow>
-      </Deck>
-    );
-  }
-
   return (
-    <div>No More Restaurants</div>
-  )
+    <Deck>
+      <TransitionGroup>
+        <CSSTransition
+          key={restaurant ? restaurant.alias : 'empty'}
+          timeout={500}
+          classNames={`card ${liked ? 'card-right' : 'card-left'}`}
+        >
+          { 
+            restaurant ? 
+            <Card restaurant={restaurant} liked={liked} disliked={disliked}/> :
+            <p>No more restaurants</p>
+          }
+        </CSSTransition>
+      </TransitionGroup>
+
+      { 
+        restaurant ?
+        <Buttons setCurrentIdx={setCurrentIdx} setLiked={setLiked} setDisliked={setDisliked}/> :
+        null 
+      }
+    </Deck>
+  );
 }
 
 CardDeck.propTypes = {
